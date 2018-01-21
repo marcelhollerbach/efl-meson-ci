ce2e1a937b2ee14bdfc73b4aa999c00230cef4d8
Marcel Hollerbach <mail@marcel-hollerbach.de>
2018-01-21 17:17:01 +0100

    ci ci ci!

--- .ci/ci-osx-build.sh
+++ .ci/ci-osx-build.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-COPTS="--with-crypto=none --disable-pulseaudio --disable-cxx-bindings"
-PARALLEL_JOBS=10
-
-# Prepare OSX env for build
-mkdir -p ~/Library/LaunchAgents
-ln -sfv /usr/local/opt/d-bus/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
-export PATH="$(brew --prefix gettext)/bin:$PATH"
-
-# Normal build test of all targets
-./autogen.sh $COPTS $@
-make -j $PARALLEL_JOBS
-#make -j $PARALLEL_JOBS examples
-#make -j $PARALLEL_JOBS benchmark
+lua ./scripts/create_all_options.lua
